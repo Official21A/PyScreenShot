7e5ab15c845a45e69874601864674df9be0a0613
Official21A <officialamirhossein21@gmail.com>
2020-10-25 08:33:42 +0330

    Simple added

--- script.py
+++ script.py
@@ -1,4 +1,22 @@
 import pyautogui
+import time
 
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save(r'new_img.png')
+
+def screenshot():
+	myScreenshot = pyautogui.screenshot()
+	myScreenshot.save(r'new_img.png')
+
+
+def wait(limit):
+	time.sleep(limit)
+
+
+def execute():
+	print("> System got executed ...")
+	wait(2)
+	screenshot()
+	print("< Screen token.")
+
+
+if __name__ == "__main__":
+	execute()	
