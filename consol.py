a1e78ec996a6f27cf1e6339463a33c15f8d3d5b6
Official21A <officialamirhossein21@gmail.com>
2020-10-26 07:54:17 +0330

    Consol refactor

--- consol.py
+++ consol.py
@@ -1,19 +1,44 @@
 import pyautogui
 import time
+import os
+import sys
+import datetime
+
+
+# This is a simple script that takes screenshots
+# form your system and will save them in its pics folder.
+# The script is just like "win+sysrq" in windows.
+
+
+DATE_FORMAT = "%b_%d_%Y-%H_%M"
+MAIN_DIR = "./pics/"
+FORMAT = ".png"
+
+
+def folder():
+	if not os.path.exists(MAIN_DIR):
+		os.mkdir(MAIN_DIR)
+
+
+def path():
+	date = datetime.datetime.now()	
+	string = date.strftime(DATE_FORMAT)	
+	return MAIN_DIR + string + FORMAT
 
 
 def screenshot():
 	myScreenshot = pyautogui.screenshot()
-	myScreenshot.save(r'new_img.png')
+	myScreenshot.save(path())
 
 
-def wait(limit):
+def wait(limit=2):
 	time.sleep(limit)
 
 
 def execute():
 	print("> System got executed ...")
-	wait(2)
+	folder()
+	wait()
 	screenshot()
 	print("< Screen token.")
 
