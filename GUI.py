b74a3a62d3e52fb0bfb71c007bfa6e01a3baada0
Official21A <officialamirhossein21@gmail.com>
2020-10-26 07:24:35 +0330

    User interface methods

--- GUI.py
+++ GUI.py
@@ -1,17 +1,32 @@
 import pyautogui
 import tkinter as tk
+from tkinter import filedialog
 
-root= tk.Tk()
 
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
+root = tk.Tk()
+
 
 def takeScreenshot ():
-    
+    global root
+    root.iconify()
+
     myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(r'new_image.png')
+    file_path = filedialog.asksaveasfilename(defaultextension='.png')
+    myScreenshot.save(file_path)
 
-myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='green',fg='white',font= 10)
-canvas1.create_window(150, 150, window=myButton)
+    root.deiconify()
 
-root.mainloop()
+
+def init():
+
+	canvas1 = tk.Canvas(root, width = 300, height = 300)
+	canvas1.pack()
+
+	myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='green',fg='white',font= 10)
+	canvas1.create_window(150, 150, window=myButton)
+
+	root.mainloop()
+
+
+if __name__ == "__main__":
+	init()	
